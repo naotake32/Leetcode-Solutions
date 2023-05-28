0546f9f0f56cb1341070c30fbd79ddff2091676c
github naotake32 <take32.nao@gmail.com>
2023-05-27 23:36:09 -0700

    solved 912. Sort an Array

---
+++ 912. Sort an Array.js
@@ -0,0 +1,63 @@
+// 912. Sort an Array
+// Medium
+// 4.9K
+// 690
+// Companies
+// Given an array of integers nums, sort the array in ascending order and return it.
+
+// You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+ 
+
+// Example 1:
+
+// Input: nums = [5,2,3,1]
+// Output: [1,2,3,5]
+// Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+// Example 2:
+
+// Input: nums = [5,1,1,2,0,0]
+// Output: [0,0,1,1,2,5]
+// Explanation: Note that the values of nums are not necessairly unique.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 5 * 104
+// -5 * 104 <= nums[i] <= 5 * 104
+// Accepted
+// 462K
+// Submissions
+// 781.5K
+// Acceptance Rate
+// 59.1%
+
+function quickSort(arr, low, high) {
+  if (low < high) {
+    let pi = partition(arr, low, high);
+    quickSort(arr, low, pi - 1);
+    quickSort(arr, pi + 1, high);
+  }
+}
+
+function partition(arr, low, high) {
+  let pivot = arr[high];
+  let i = low - 1;
+  for (let j = low; j <= high - 1; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+  let temp = arr[i + 1];
+  arr[i + 1] = arr[high];
+  arr[high] = temp;
+  return i + 1;
+}
+
+function sortArray(nums) {
+  quickSort(nums, 0, nums.length - 1);
+  return nums;
+}
